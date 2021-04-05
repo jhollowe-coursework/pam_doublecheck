269f447f88659e65577355c0462f3af88c1eafd0
John Hollowell <johnahollowell@gmail.com>
2021-04-05 15:38:12 -0400

    Add dummy values for Twilio creds

--- secrets.h
+++ secrets.h
@@ -1,3 +1,5 @@
 // TODO separate these into a separate config file
 // NOTE: these are demo values and will not actually work with Twilio
-#define DC_TWILIO_VERBOSE true
+#define DC_TWILIO_SID     "ACb3da7e73f864b3cc70b44c3fff77728d"
+#define DC_TWILIO_AUTH    "2c05550bed1c4445d262cbd592acc050"
+#define DC_TWILIO_FROM    "+1111111111"
